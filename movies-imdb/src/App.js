3da18e19ac164fec3e2c3726203963e0434a20fb
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-07-25 21:53:17 +0430

    Add Footer Component To The Project

--- movies-imdb/src/App.js
+++ movies-imdb/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Main/Header/Header";
 import Home from "./Components/Main/Home/Home";
+import Footer from "./Components/Main/Footer/Footer";
 
 const App = () => {
     return (
         <>
             <Header />
             <Home />
+            <Footer />
         </>
     );
 };
---
+++ movies-imdb/src/Components/Main/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="container-fluid">
+                <div className="col-lg-1 col-md-2 col-sm-3 col-4">
+                    social Medias
+                    <i className="fa fa-instagram" />
+                    <i className="fa fa-telegram" />
+                    <i className="fa fa-twitter" />
+                    <i className="fa fa-envelope-o" />
+                </div>
+                {/* totop */}
+                {/* movies */}
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
