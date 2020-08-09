1902cf0424cbc6349b2748768af4ed40735b5468
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-09 19:58:30 +0430

    Fetching The API for Each Movie

--- movies-imdb/src/Components/Main/MoviePage/MoviePage.jsx
+++ movies-imdb/src/Components/Main/MoviePage/MoviePage.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import Navigation from "./Navigation/Navigation";
+import { apiURL, apiKey } from "../../../Services/Config.json";
 
-const MoviePage = () => {
-    return (
-        <>
-            <div className="container-fluid">
-                <Navigation />
-            </div>
-        </>
-    );
-};
+class MoviePage extends Component {
+    state = {
+        movie: [],
+        director: [],
+        actors: [],
+    };
+    componentDidMount() {
+        const { match } = this.props;
+
+        const movie = `${apiURL}movie/${match.params.id}?api_key=${apiKey}&language=en-us`;
+        this.fetchData(movie);
+    }
+    fetchData = (movie) => {
+        const { match } = this.props;
+
+        fetch(movie)
+            .then((movie) => movie.json())
+            .then((movie) => {
+                const data = `${apiURL}movie/${match.params.id}/credits?api_key=${apiKey}`;
+                fetch(data)
+                    .then((people) => people.json())
+                    .then((people) => {
+                        const director = people.crew.filter(
+                            (people) => people.job === "Director"
+                        );
+                        this.setState({
+                            movie: movie,
+                            director: director,
+                            actors: people.cast,
+                        });
+                    });
+            });
+    };
+    render() {
+        return (
+            <>
+                <div className="container-fluid">
+                    <Navigation />
+                </div>
+            </>
+        );
+    }
+}
 
 export default MoviePage;
--- movies-imdb/src/Components/Main/MoviePage/Navigation/Navigation.jsx
+++ movies-imdb/src/Components/Main/MoviePage/Navigation/Navigation.jsx
@@ -1,9 +1,8 @@
 import React from "react";
+// import { apiURL, apiKey } from "../../../../Services/Config.json";
 
-const Navigation = () => {
-    return <>
-    div.
-    </>;
+const Navigation = ({ movieID }) => {
+    return <></>;
 };
 
 export default Navigation;
