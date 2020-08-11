854b47bef59403691c68d0b734d8ae750dab895f
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-11 19:50:21 +0430

    Refactoring The Sort Component

--- movies-imdb/src/Components/Common/Sort/Sort.jsx
+++ movies-imdb/src/Components/Common/Sort/Sort.jsx
@@ -1,63 +1,68 @@
 import React from "react";
 
 const Sort = ({ sortColumn, onSort }) => {
+    const sorts = [
+        {
+            path: "title",
+            labelAsc: "A to Z",
+            labelDesc: "Z to A",
+            className: "btn btn-warning m-2",
+            classNameIconAsc: "fas fa-sort-alpha-down",
+            classNameIconDesc: "fas fa-sort-alpha-down-alt",
+        },
+        {
+            path: "popularity",
+            labelAsc: "Least Popular",
+            labelDesc: "Most Popular",
+            className: "btn btn-danger m-2",
+            classNameIconAsc: "fas fa-sort-amount-down-alt",
+            classNameIconDesc: "fas fa-sort-amount-down",
+        },
+        {
+            path: "vote_average",
+            labelAsc: "Worst Rating",
+            labelDesc: "Best Rating",
+            className: "btn btn-dark m-2",
+            classNameIconAsc: "fas fa-sort-numeric-down",
+            classNameIconDesc: "fas fa-sort-numeric-down-alt",
+        },
+    ];
     const handleSort = (column) => {
-        const allSortColumn = { ...sortColumn };
         if (sortColumn.path === column) {
-            allSortColumn.order =
-                allSortColumn.order === "asc" ? "desc" : "asc";
+            sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
         } else {
-            allSortColumn.path = column;
-            allSortColumn.order = "desc";
+            sortColumn.path = column;
+            sortColumn.order = "desc";
         }
-        onSort(allSortColumn);
+        onSort(sortColumn);
     };
-    const handleIcons = () => {
-        let titleIcon = "fas fa-sort-alpha-down";
-        if (sortColumn.path === "title") {
-            if (sortColumn.order === "desc") {
-                titleIcon += "-alt";
-            }
-        }
-        let popularityIcon = "fas fa-sort-amount-down-alt";
-        if (sortColumn.path === "popularity") {
-            if (sortColumn.order === "desc") {
-                popularityIcon = "fas fa-sort-amount-down";
-            }
-        }
-        let voteAverageIcon = "fas fa-sort-numeric-down";
-        if (sortColumn.path === "vote_average") {
-            if (sortColumn.order === "desc") {
-                voteAverageIcon += "-alt";
-            }
-        }
-        return { titleIcon, popularityIcon, voteAverageIcon };
-    };
-    const { titleIcon, popularityIcon, voteAverageIcon } = handleIcons();
 
     return (
         <>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-12 col-md-12 col-sm-12 col-12 sort">
-                        <button
-                            className="btn"
-                            onClick={() => handleSort("title")}
-                        >
-                            <i className={titleIcon} />
-                        </button>
-                        <button
-                            className="btn"
-                            onClick={() => handleSort("popularity")}
-                        >
-                            <i className={popularityIcon} />
-                        </button>
-                        <button
-                            className="btn"
-                            onClick={() => handleSort("vote_average")}
-                        >
-                            <i className={voteAverageIcon} />
-                        </button>
+                    <div className="sort col-lg-12 col-md-12 col-sm-12 col-12">
+                        {sorts.map((sort) => (
+                            <button
+                                className={sort.className}
+                                key={sort.path}
+                                onClick={() => handleSort(sort.path)}
+                            >
+                                <i
+                                    className={
+                                        sortColumn.path === sort.path &&
+                                        sortColumn.order === "asc"
+                                            ? sort.classNameIconAsc
+                                            : sort.classNameIconDesc
+                                    }
+                                />
+                                <h6>
+                                    {sortColumn.path === sort.path && sortColumn.order === "asc"
+                                        ? sort.labelAsc
+                                        : sort.labelDesc}
+                                </h6>
+                            </button>
+                        ))}
                     </div>
                 </div>
             </div>
--- movies-imdb/src/Components/Main/Home/Home.jsx
+++ movies-imdb/src/Components/Main/Home/Home.jsx
@@ -19,7 +19,7 @@ class Home extends Component {
         searchQuery: "",
         currentPage: 1,
         isLoading: false,
-        sortColumn: { path: "", order: "asc" },
+        sortColumn: { path: "title", order: "asc" },
     };
 
     async componentDidMount() {
