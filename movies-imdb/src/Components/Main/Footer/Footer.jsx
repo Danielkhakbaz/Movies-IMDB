bfeca930bffa1338c0373aa7e3d53afe51811eea
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-11 16:18:13 +0430

    Refactoring The Footer Component

--- movies-imdb/src/Components/Main/Footer/Footer.jsx
+++ movies-imdb/src/Components/Main/Footer/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Footer = () => {
     return (
         <>
-        
             <footer className="container-fluid">
                 <div className="footer__links row p-2">
                     <a
