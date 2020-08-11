c04d7988e128d8c35f566f771a3a90702ef3b1e4
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-11 16:36:40 +0430

    Refactoring The Poster Component

--- movies-imdb/src/Components/Main/Home/Poster/Poster.jsx
+++ movies-imdb/src/Components/Main/Home/Poster/Poster.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Poster = () => {
     return (
         <>
-            <section className="poster">
-                <h1 className="poster__h1 m-4">Avengers: Infinity War</h1>
-                <p className="poster__p m-4">
+            <section className="poster container-fluid">
+                <h1 className="poster__title m-2">Avengers: Infinity War</h1>
+                <p className="poster__overview m-2">
                     As the Avengers and their allies have continued to protect
                     the world from threats too large for any one hero to handle,
                     a new danger has emerged from the cosmic shadows: Thanos. A
