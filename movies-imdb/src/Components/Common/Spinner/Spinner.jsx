33edc153db6f8c57b250994f8911b5b9019962dc
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-12 17:37:35 +0430

    Refactoring The Spinner Component

--- movies-imdb/src/Components/Common/Spinner/Spinner.jsx
+++ movies-imdb/src/Components/Common/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => {
     return (
         <>
-            <div className="loading"></div>
+            <div className="loading__icon"></div>
         </>
     );
 };
