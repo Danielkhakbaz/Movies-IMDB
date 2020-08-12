faacb0460a866989ff8f35b0bc588e0e7e47884d
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-12 14:11:38 +0430

    Refactoring The SearchBox Component

--- movies-imdb/src/Components/Common/SearchBox/SearchBox.jsx
+++ movies-imdb/src/Components/Common/SearchBox/SearchBox.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const SearchBox = ({ value, onChange }) => {
     return (
         <>
-            <nav className="navbar searchbox">
+            <nav className="searchbox navbar">
                 <input
                     className="form-control m-3"
                     type="search"
                     value={value}
                     onChange={(e) => onChange(e.target.value)}
-                    placeholder="Search for your Favorite Movie..."
+                    placeholder="Search for Movies..."
                 />
             </nav>
         </>
