efad35c7f4a2c384d7f0e261c304aaadd479b3b9
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-12 17:53:11 +0430

    Implement PropTypes

--- movies-imdb/src/Components/Common/SearchBox/SearchBox.jsx
+++ movies-imdb/src/Components/Common/SearchBox/SearchBox.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const SearchBox = ({ value, onChange }) => {
     return (
@@ -14,6 +15,11 @@ const SearchBox = ({ value, onChange }) => {
             </nav>
         </>
     );
+};
+
+SearchBox.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
 };
 
 export default SearchBox;
