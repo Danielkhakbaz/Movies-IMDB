3a6a15ecf12ffa00440f8ab917b35cdbb6ee5e0c
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2020-08-24 18:52:25 +0430

    Refactored The NotFound Component

--- movies-imdb/src/Components/Main/NotFound/NotFound.jsx
+++ movies-imdb/src/Components/Main/NotFound/NotFound.jsx
@@ -4,13 +4,9 @@ const NotFound = () => {
     return (
         <>
             <section className="container-fluid">
-                <div className="notfound row">
-                    <h1 className="notfound__title col-lg-12 col-md-12 col-sm-12 col-12">
-                        Not Found
-                    </h1>
-                    <h1 className="notfound__title col-lg-12 col-md-12 col-sm-12 col-12">
-                        404
-                    </h1>
+                <div className="row notfound">
+                    <h1 className="notfound__title">Not Found</h1>
+                    <h1 className="notfound__title">404</h1>
                 </div>
             </section>
         </>
